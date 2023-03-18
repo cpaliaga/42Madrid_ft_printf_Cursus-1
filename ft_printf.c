c0a25711ffac7d3b2f44e3bfa8e1bb17f0232ed9
Cristina Patricia Aliaga Jimenez <caliaga-@c3r13s6.42madrid.com>
2023-03-18 17:13:35 +0100

    hey

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: caliaga- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 17:59:13 by caliaga-          #+#    #+#             */
-/*   Updated: 2023/03/18 16:31:52 by caliaga-         ###   ########.fr       */
+/*   Updated: 2023/03/18 17:12:41 by caliaga-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 #include <stdarg.h>  /* va_list */
 #include <stdint.h> /* uint */
 
-void ft_pchar(int c)
+void	ft_pchar(int c)
 {
-    write(1, &c, 1);
+	write(1, &c, 1);
 }
 
 size_t	ft_strlen(char *str)
@@ -30,135 +30,133 @@ size_t	ft_strlen(char *str)
 	return (a);
 }
 
-void ft_pchain(char *chain)
+void	ft_pchain(char *chain)
 {
-    if (!chain)
-        write(1, "(null)", 6);
-    else
-        write(1, chain, ft_strlen(chain));
+	if (!chain)
+		write(1, "(null)", 6);
+	else
+		write(1, chain, ft_strlen(chain));
 }
 
-size_t ft_reverse(unsigned int nb)
+size_t	ft_reverse(unsigned int nb)
 {
-    size_t rev;
-    rev = 0;
+	size_t	rev;
 
-    while (nb > 9)
-    {
-        rev += nb % 10;
-        rev *= 10;
-        nb /= 10;
-    }
-    rev += nb % 10;
-
-    return(rev);
+	rev = 0;
+	while (nb > 9)
+	{
+		rev += nb % 10;
+		rev *= 10;
+		nb /= 10;
+	}
+	rev += nb % 10;
+	return (rev);
 }
 
-void ft_pnumber(int n)
+void	ft_pnumber(int n)
 {
 	if (n == -2147483648)
-        write(1, "-2147483648", 11);
+		write(1, "-2147483648", 11);
 	else
-    {
-        if (n < 0)
-        {
-            ft_pnumber(n = n * -1); 
-            ft_pchar('-');         
-        }
-        else
-        {
-            n = ft_reverse(n);
-            while (n > 0)
-            {
-                ft_pchar(n % 10 + '0');
-                n /= 10;
-            }
-        }
-    }
+	{
+		if (n < 0)
+		{
+			ft_pnumber(n = n * -1);
+			ft_pchar('-');
+		}
+		else
+		{
+			n = ft_reverse(n);
+			while (n > 0)
+			{
+				ft_pchar(n % 10 + '0');
+				n /= 10;
+			}
+		}
+	}
 }
 
-void ft_unsigned(unsigned int nb)
+void	ft_unsigned(unsigned int nb)
 {
-    nb = ft_reverse(nb);
-    while (nb > 0)
-    {
-        ft_pchar(nb % 10 + '0');
-        nb /= 10;
-    }
+	nb = ft_reverse(nb);
+	while (nb > 0)
+	{
+		ft_pchar(nb % 10 + '0');
+		nb /= 10;
+	}
 }
-/*
-void ft_hex(unsigned int h,char bs){
-    char *base; 
 
-    if (bs == 'X')
-        base = "0123456789ABCDEF";
-    else
-        base = "0123456789abcdef";
-    
+void	ft_hex(unsigned int h, char bs)
+{
+	char	*base;
 
+	if (bs == 'X')
+		base = "0123456789ABCDEF";
+	else
+		base = "0123456789abcdef";
 }
-*/
+/**/
 /*void ft_pnt(){}*/
 
-void print(const char* phase, ...)
+void	print(const char *phase, ...)
 {
-    va_list args;
-    va_start(args, phase);
-    while (*phase) {
-        if (*phase == '%') 
-        {
-            if (*(phase + 1) == 'c') /* EL ARGUMENTO ES UN CARACTER */
-            {
-                ft_pchar(va_arg(args, int));
-                phase++;
-            }                
-            else if (*(phase + 1) == '%') /* EL ARGUMENTO ES UN PORCENTAJE */
-            {
-                ft_pchar('%');
-                phase++;
-            } 
-            else if (*(phase + 1) == 's') /*EL ARGUMENTO ES UNA CADENA*/
-            {
-                ft_pchain(va_arg(args, char*));
-                phase++;
-            }
-            else if (*(phase + 1) == 'd' || *(phase + 1) == 'i') //EL ARGUMENTO ES UN ENTERO 
-            {
-                ft_pnumber(va_arg(args, int));
-                phase++;
-            }
-            else if (*(phase + 1) == 'u') //EL ARGUMENTO ES UN ENTERO SIN SIGNO
-            {
-                ft_unsigned(va_arg(args, unsigned int));
-                phase++;
-            }
-            /*
-            else if (*(phase + 1) == 'x' || *(phase + 1) == 'X') //EL ARGUMENTO ES UN HEXADECIMAL
-            {
-                ft_hex(va_arg(args, unsigned int), *(phase + 1));
-                phase++;
-            }
-            else if (*(phase + 1) == 'p') //EL ARGUMENTO ES UN PUNTERO           {
-                ft_pnt(va_arg(args, unsigned int), *(phase + 1));
-                phase++;
-            }
-            */
-        }
-        else
-        {
-            write(1,phase,1);
-        }
-        phase++;
-    }
-    va_end(args);
+	va_list	args;
+
+	va_start (args, phase);
+	while (*phase)
+	{
+		if (*phase == '%')
+		{
+			if (*(phase + 1) == 'c') // EL ARGUMENTO ES UN CARACTER
+			{
+				ft_pchar(va_arg(args, int));
+				phase++;
+			}
+			else if (*(phase + 1) == '%') // EL ARGUMENTO ES UN PORCENTAJE
+			{
+				ft_pchar('%');
+				phase++;
+			}
+			else if (*(phase + 1) == 's') // EL ARGUMENTO ES UNA CADENA
+			{
+				ft_pchain(va_arg (args, char *));
+				phase++;
+			}
+			else if (*(phase + 1) == 'd' || *(phase + 1) == 'i') //EL ARGUMENTO ES UN ENTERO 
+			{
+				ft_pnumber(va_arg(args, int));
+				phase++;
+			}
+			else if (*(phase + 1) == 'u') //EL ARGUMENTO ES UN ENTERO SIN SIGNO
+			{
+				ft_unsigned(va_arg(args, unsigned int));
+				phase++;
+			}
+			else if (*(phase + 1) == 'x' || *(phase + 1) == 'X') //EL ARGUMENTO ES UN HEXADECIMAL
+			{
+				ft_hex(va_arg(args, unsigned int), *(phase + 1));
+				phase++;
+			}
+            else if (*(phase + 1) == 'p') //EL ARGUMENTO ES UN PUNTERO
+			{
+				//ft_pnt(va_arg(args, unsigned int), *(phase + 1));
+				//phase++;
+			}
+		}
+		else
+		{
+			write(1, phase, 1);
+		}
+		phase++;
+	}
+	va_end(args);
 }
 
-int main(void)
+int	main(void)
 {
-    print("Hola bienvenido a %s, %% estamos en %i - %u \n", "hardfloat.es", 2021, 1234567891);
-    print("Hola bienvenido a %s, %% estamos en %d\n", "hardfloat.es", 2021);
-    return 0;
-    return (0);
+	print("Hola bienvenido a %s, %% estamos en %i - %u \n", "hardfloat.es", 2021, 1234567891);
+	print("Hola bienvenido a hexadecimal %x en HEXADECIMAL %X \n", 3456, 2021);
+	return (0);
 }
 
 /* Deberás implementar las siguientes conversiones: cspdiuxX % */
